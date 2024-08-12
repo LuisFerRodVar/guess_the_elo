64ca10cc0c795c796020d14fdffff3a7d3ee13eb
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-08-11 21:41:43 -0600

    Added main components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { EndComponent } from './end/end.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "game",
+    component: GameComponent
+  },
+  {
+    path: "result",
+    component: EndComponent
+  }
+];
