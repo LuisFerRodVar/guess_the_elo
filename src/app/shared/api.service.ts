4f3f350a3345d0c071dc922b1bc33915b417088e
LuisFerRodVar <luisfrv1@estudiantec.cr>
2025-05-26 14:28:43 -0600

    updated api

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -8,7 +8,7 @@ import { User } from './user';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'https://api-guess-the-elo.shuttleapp.rs';
+  private baseUrl = 'https://api-guess-the-elo-9kiw.shuttle.app/';
 
   constructor(private http: HttpClient) { }
 
